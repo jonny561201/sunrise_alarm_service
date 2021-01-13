da3eb719117d5a5880f80b1243df00f799b860c2
Jon Graf <jongraf1@gmail.com>
2021-01-12 18:44:56 -0600

    added deployment folder with service file and start service script

---
+++ deployment/startService.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+YELLOW='\033[1;33m'
+WHITE='\033[0m'
+RED='\033[0;31m'
+
+SUNRISE_SERVICE_FILE=sunriseAlarm.service
+
+
+function cloneServiceFiles {
+    if [[ -d "/home/pi/sunrise_alarm_service" ]]; then
+        echo -e "${YELLOW}---------------Service Folder Exists---------------${WHITE}"
+        cd /home/pi/sunrise_alarm_service
+        git pull
+    else
+        echo -e "${YELLOW}---------------Cloning Service---------------${WHITE}"
+        cd /home/pi/
+        git clone https://github.com/jonny561201/sunrise_alarm_service.git /home/pi/sunrise_alarm_service
+    fi
+}
+
+function installDependencies {
+    echo -e "${YELLOW}---------------Installing Dependencies---------------${WHITE}"
+    pip3 install -Ur requirements.txt
+}
+
+function stopService {
+    echo -e "${YELLOW}---------------Stopping Service---------------${WHITE}"
+    sudo systemctl stop ${SUNRISE_SERVICE_FILE}
+    sudo rm /lib/systemd/system/${SUNRISE_SERVICE_FILE}
+}
+
+function copyServiceFile {
+    echo  -e "${YELLOW}---------------Creating SystemD---------------${WHITE}"
+    sudo chmod 666 ./deployment/${SUNRISE_SERVICE_FILE}
+    sudo yes | sudo cp ./deployment/${SUNRISE_SERVICE_FILE} /lib/systemd/system/${SUNRISE_SERVICE_FILE}
+}
+
+function configureSystemD {
+    echo  -e "${YELLOW}---------------Configuring SystemD---------------${WHITE}"
+    sudo systemctl daemon-reload
+    sudo systemctl enable ${SUNRISE_SERVICE_FILE}
+}
+
+function restartDevice {
+    echo  -e "${YELLOW}---------------Rebooting Device---------------${WHITE}"
+    sudo reboot
+}
+
+function createEnvironmentVariableFile {
+    if [[ ! -f "/home/pi/sunrise_alarm_service/serviceEnvVariables" ]]; then
+        echo -e "${YELLOW}---------------Creating Environment Variable File---------------${WHITE}"
+        createFile
+    else
+        echo -e "${YELLOW}---------------Environment Variable File Already Exists---------------${WHITE}"
+        echo 'Would you like to recreate serviceEnvVariables file? (y/n)'
+        read USER_RESPONSE
+        if [[ ${USER_RESPONSE} == "y" ]]; then
+            createFile
+        fi
+    fi
+    echo -e "${YELLOW}---------------Exporting Environment Variables---------------${WHITE}"
+    set -o allexport; source serviceEnvVariables; set +o allexport
+}
+
+function createFile {
+    echo -e "Enter JWT_SECRET:${WHITE}"
+    read JWT_SECRET
+
+    echo "JWT_SECRET=${JWT_SECRET}" > serviceEnvVariables
+}
+
+
+stopService
+cloneServiceFiles
+installDependencies
+createEnvironmentVariableFile
+copyServiceFile
+configureSystemD
+restartDevice
