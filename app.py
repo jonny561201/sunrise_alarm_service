68739b5df05f72fb5fdc34035ef576f4f790cc12
Jon Graf <jongraf1@gmail.com>
2021-01-15 20:19:04 -0600

    updated the entry point of the app to schedule a thread to constantly check the alarm list for changes to update

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import logging
 
+from svc.constants.home_automation import Automation
+from svc.constants.thread_state import ThreadState
 from svc.services.light_service import create_light_alarm
+from svc.utilities.event_utils import create_thread
 
 logging.basicConfig(filename='sunriseAlarm.log', level=logging.DEBUG)
 
 try:
     logging.info('Application started!')
     create_light_alarm()
+    create_thread(ThreadState(), create_light_alarm, Automation.TIME.ONE_MINUTE)
 except Exception:
     logging.error('Application interrupted by user')
