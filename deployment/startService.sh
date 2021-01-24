a1eee7f0575ff862db327a249317b71564f3cfa0
Jon Graf <jongraf1@gmail.com>
2021-01-24 17:08:58 -0600

    added deployment environment variables to startup script

--- deployment/startService.sh
+++ deployment/startService.sh
@@ -64,10 +64,19 @@ function createEnvironmentVariableFile {
 }
 
 function createFile {
-    echo -e "Enter JWT_SECRET:${WHITE}"
-    read JWT_SECRET
+    echo -e "Enter LIGHT_API_USERNAME:${WHITE}"
+    read LIGHT_USER
+    echo -e "Enter LIGHT_API_PASSWORD:${WHITE}"
+    read LIGHT_PASS
+    echo -e "Enter USER_ID:${WHITE}"
+    read USER_ID
+    echo -e "Enter HUB_BASE_URL:${WHITE}"
+    read HUB_BASE_URL
 
-    echo "JWT_SECRET=${JWT_SECRET}" > serviceEnvVariables
+    echo "LIGHT_API_USERNAME=${LIGHT_USER}" > serviceEnvVariables
+    echo "LIGHT_API_PASSWORD=${LIGHT_PASS}" >> serviceEnvVariables
+    echo "USER_ID=${USER_ID}" >> serviceEnvVariables
+    echo "HUB_BASE_URL=${HUB_BASE_URL}" >> serviceEnvVariables
 }
 
 
