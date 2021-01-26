eebfd67c0893b54a0ae295c3650553bbff70dede
Jon Graf <jongraf1@gmail.com>
2021-01-25 18:56:20 -0600

    light on program will now not turn on light when before start timer

--- tests/unit/utilities/test_light_utils.py
+++ tests/unit/utilities/test_light_utils.py
@@ -94,4 +94,9 @@ class TestLightUtils:
 
         mock_api.assert_not_called()
 
+    def test_light_on_program__should_not_turn_on_light_just_before_timer(self, mock_api, mock_date):
+        before_time = datetime.time(7, 29, 59)
+        mock_date.datetime.now.return_value = before_time
+        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
 
+        mock_api.assert_not_called()
