04688a45b67c7ec64721ae79b88bc1417bf28a30
Jon Graf <jongraf1@gmail.com>
2021-01-25 19:19:04 -0600

    broke out the light on alarm to a different state object as little overlap

--- svc/constants/thread_state.py
+++ svc/constants/thread_state.py
@@ -11,8 +11,8 @@ class LightAlarmState(ThreadState):
     ALARM_START_TIME = None
     ALARM_STOP_TIME = None
     ALARM_DAYS = None
+    #TODO: can I delete group id????
     LIGHT_GROUP_ID = None
-    TRIGGERED = False
 
     def __init__(self, task_id: str, light_group_id: str, alarm_time: datetime.time, alarm_days: str):
         self.THREAD_ID = task_id
@@ -20,3 +20,14 @@ class LightAlarmState(ThreadState):
         self.LIGHT_GROUP_ID = light_group_id
         self.ALARM_START_TIME = (datetime.datetime.combine(datetime.date.today(), alarm_time) + datetime.timedelta(minutes=-10)).time()
         self.ALARM_STOP_TIME = (datetime.datetime.combine(datetime.date.today(), alarm_time) + datetime.timedelta(minutes=+10)).time()
+
+
+class LightOnState(ThreadState):
+    TRIGGERED = False
+    ALARM_START_TIME = None
+    ALARM_DAYS = None
+
+    def __init__(self, task_id: str, alarm_time: datetime.time, alarm_days: str):
+        self.THREAD_ID = task_id
+        self.ALARM_DAYS = alarm_days
+        self.ALARM_START_TIME = datetime.datetime.combine(datetime.date.today(), alarm_time).time()
--- tests/unit/utilities/test_light_utils.py
+++ tests/unit/utilities/test_light_utils.py
@@ -3,7 +3,9 @@ import datetime
 from mock import patch
 
 from svc.constants.lights_state import LightAlarmState
+from svc.constants.thread_state import LightOnState
 from svc.utilities.light_utils import light_alarm_program, light_on_program
+
 
 
 @patch('svc.utilities.light_utils.datetime')
@@ -11,6 +13,7 @@ from svc.utilities.light_utils import light_alarm_program, light_on_program
 class TestLightUtils:
     GROUP_ID = '3'
     TASK_ID = 'abc123'
+    DAYS = 'MonTueFri'
     API_KEY = 'test_api_key'
     START_TIME = datetime.time(7, 30, 0)
     END_TIME = datetime.time(7, 40, 0)
@@ -19,98 +22,99 @@ class TestLightUtils:
     WEDNESDAY = datetime.datetime(2020, 11, 4, 7, 34, 0)
 
     def setup_method(self):
-        self.LIGHTS = LightAlarmState(self.TASK_ID, self.GROUP_ID, self.START_TIME, 'MonTueFri')
-        self.LIGHTS.ALARM_COUNTER = 0
-        self.LIGHTS.ALARM_START_TIME = self.START_TIME
-        self.LIGHTS.ALARM_STOP_TIME = self.END_TIME
+        self.LIGHT_ON = LightOnState(self.TASK_ID, self.START_TIME, self.DAYS)
+        self.ALARM = LightAlarmState(self.TASK_ID, self.GROUP_ID, self.START_TIME, self.DAYS)
+        self.ALARM.ALARM_COUNTER = 0
+        self.ALARM.ALARM_START_TIME = self.START_TIME
+        self.ALARM.ALARM_STOP_TIME = self.END_TIME
 
     def test_run_light_program__should_call_set_light_group_if_after_start_time(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = datetime.datetime(2020, 11, 2, 7, 31, 0)
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_called_with(self.API_KEY, self.GROUP_ID, True, 2)
 
     def test_run_light_program__should_call_set_light_group_if_equal_start_time(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = datetime.datetime(2020, 11, 2, 7, 30, 0)
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_called_with(self.API_KEY, self.GROUP_ID, True, 2)
 
     def test_run_light_program__should_not_call_set_light_group_if_before_start_time(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = datetime.datetime(2020, 11, 2, 7, 29, 0)
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_run_light_program__should_not_call_set_light_group_if_after_end_time(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = datetime.datetime(2020, 11, 2, 7, 41, 0)
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_run_light_program__should_increment_the_light_brightness(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.MONDAY
-        self.LIGHTS.ALARM_COUNTER = 121
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        self.ALARM.ALARM_COUNTER = 121
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_called_with(self.API_KEY, self.GROUP_ID, True, 123)
 
     def test_run_light_program__should_reset_the_light_state_value_back_to_zero(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = datetime.datetime(2020, 11, 2, 7, 41, 0)
-        self.LIGHTS.ALARM_CURRENT_STATE = 203
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        self.ALARM.ALARM_CURRENT_STATE = 203
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
-        assert self.LIGHTS.ALARM_COUNTER == 0
+        assert self.ALARM.ALARM_COUNTER == 0
 
     def test_run_light_program__should_stop_incrementing_once_reach_255(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.MONDAY
-        self.LIGHTS.ALARM_COUNTER = 255
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        self.ALARM.ALARM_COUNTER = 255
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_run_light_program__should_not_call_set_light_group_when_sun(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.SUNDAY
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_run_light_program__should_not_call_set_light_group_when_wed(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.WEDNESDAY
-        light_alarm_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_alarm_program(self.ALARM, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_light_on_program__should_turn_light_to_max_when_after_timer_on_matching_day(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.MONDAY
-        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_on_program(self.LIGHT_ON, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_called_with(self.API_KEY, self.GROUP_ID, True, 0)
 
     def test_light_on_program__should_not_turn_light_to_max_when_after_timer_on_wrong_day(self, mock_api, mock_date):
         mock_date.datetime.now.return_value = self.SUNDAY
-        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_on_program(self.LIGHT_ON, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_light_on_program__should_not_turn_on_light_just_before_timer(self, mock_api, mock_date):
         before_time = self.MONDAY + datetime.timedelta(minutes=-4, seconds=-1)
         mock_date.datetime.now.return_value = before_time
-        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_on_program(self.LIGHT_ON, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
 
     def test_light_on_program__should_turn_on_light_when_exactly_at_timer(self, mock_api, mock_date):
         exact_time = self.MONDAY + datetime.timedelta(minutes=-4)
         mock_date.datetime.now.return_value = exact_time
-        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_on_program(self.LIGHT_ON, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_called_with(self.API_KEY, self.GROUP_ID, True, 0)
 
     def test_light_on_program__should_not_turn_on_light_after_already_turning_on(self, mock_api, mock_date):
-        self.LIGHTS.TRIGGERED = True
+        self.LIGHT_ON.TRIGGERED = True
         mock_date.datetime.now.return_value = self.MONDAY
-        light_on_program(self.LIGHTS, self.API_KEY, self.GROUP_ID)
+        light_on_program(self.LIGHT_ON, self.API_KEY, self.GROUP_ID)
 
         mock_api.assert_not_called()
