813387a14febbbf2bd0b10a6f9a4226677011e13
Jon Graf <jongraf1@gmail.com>
2021-01-26 18:17:44 -0600

    update light groups will now check if all light groups, but case insensitively

--- svc/services/api_service.py
+++ svc/services/api_service.py
@@ -2,5 +2,5 @@ from svc.utilities.api_utils import get_light_groups
 
 
 def update_light_groups(api_key, group_id, on):
-    if group_id == 'ALL':
+    if group_id.upper() == 'ALL':
         get_light_groups(api_key)
--- tests/unit/services/test_api_service.py
+++ tests/unit/services/test_api_service.py
@@ -20,3 +20,8 @@ class TestApiService:
         update_light_groups(self.API_KEY, self.GROUP_ID, self.ON_STATE)
 
         mock_api.assert_not_called()
+
+    def test_update_light_groups__should_not_call_to_set_light_groups_when_single_group_id_case_insensitive(self, mock_api):
+        update_light_groups(self.API_KEY, 'all', self.ON_STATE)
+
+        mock_api.assert_called_with(self.API_KEY)
