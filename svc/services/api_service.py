30a57163e9574bd3ee8c7035d2be2c281a6746af
Jon Graf <jongraf1@gmail.com>
2021-01-26 18:27:09 -0600

    update light groups will get list of group ids and call set light groups for each group id

--- svc/services/api_service.py
+++ svc/services/api_service.py
@@ -1,6 +1,9 @@
-from svc.utilities.api_utils import get_light_groups
+from svc.utilities import api_utils
 
 
 def update_light_groups(api_key, group_id, on):
     if group_id.upper() == 'ALL':
-        get_light_groups(api_key)
+        groups = api_utils.get_light_groups(api_key)
+        group_ids = [group_id for group_id, data in groups.items()]
+        for id in group_ids:
+            api_utils.set_light_groups(api_key, id, on, 255)
--- tests/unit/services/test_api_service.py
+++ tests/unit/services/test_api_service.py
@@ -3,7 +3,7 @@ from mock import patch
 from svc.services.api_service import update_light_groups
 
 
-@patch('svc.services.api_service.get_light_groups')
+@patch('svc.services.api_service.api_utils')
 class TestApiService:
     API_KEY = 'asjdfhasiw'
     GROUP_ID = '2'
@@ -11,17 +11,25 @@ class TestApiService:
     ON_STATE = True
     OFF_STATE = False
 
-    def test_update_light_groups__should_call_to_set_light_groups_when_all_group_id(self, mock_api):
+    def test_update_light_groups__should_call_to_get_light_groups_when_all_group_id(self, mock_api):
         update_light_groups(self.API_KEY, self.ALL_GROUPS_ID, self.ON_STATE)
 
-        mock_api.assert_called_with(self.API_KEY)
+        mock_api.get_light_groups.assert_called_with(self.API_KEY)
 
-    def test_update_light_groups__should_not_call_to_set_light_groups_when_single_group_id(self, mock_api):
+    def test_update_light_groups__should_not_call_to_get_light_groups_when_single_group_id(self, mock_api):
         update_light_groups(self.API_KEY, self.GROUP_ID, self.ON_STATE)
 
-        mock_api.assert_not_called()
+        mock_api.get_light_groups.assert_not_called()
 
-    def test_update_light_groups__should_not_call_to_set_light_groups_when_single_group_id_case_insensitive(self, mock_api):
+    def test_update_light_groups__should_call_to_get_light_groups_when_all_group_id_case_insensitive(self, mock_api):
         update_light_groups(self.API_KEY, 'all', self.ON_STATE)
 
-        mock_api.assert_called_with(self.API_KEY)
+        mock_api.get_light_groups.assert_called_with(self.API_KEY)
+
+    def test_update_light_groups__should_call_to_set_all_light_groups_when_all_group_id_and_on_requested(self, mock_api):
+        mock_api.get_light_groups.return_value = {'1': {'name': 'Living Room'}, '2': {'name': 'Kitchen'}}
+        update_light_groups(self.API_KEY, 'all', self.ON_STATE)
+
+        assert mock_api.set_light_groups.call_count == 2
+        mock_api.set_light_groups.assert_any_call(self.API_KEY, '1', True, 255)
+        mock_api.set_light_groups.assert_any_call(self.API_KEY, '2', True, 255)
