36099bf99ef3e3b4473eab79ce4d960907281c3e
Jon Graf <jongraf1@gmail.com>
2021-01-28 10:03:28 -0600

    modified the light on and off threads to run every ten seconds now

--- svc/constants/lights_state.py
+++ svc/constants/lights_state.py
@@ -29,12 +29,12 @@ class LightState:
                 self.LIGHT_ALARMS.append(alarm)
             elif task_type == Automation.LIGHT.TURN_ON:
                 alarm = LightOnOffState(task_id, alarm_time, alarm_days)
-                alarm.ACTIVE_THREAD = create_thread(lambda: light_on_program(alarm, self.get_light_api_key(), light_group_id), Automation.TIME.TWENTY_SECONDS)
+                alarm.ACTIVE_THREAD = create_thread(lambda: light_on_program(alarm, self.get_light_api_key(), light_group_id), Automation.TIME.TEN_SECONDS)
                 alarm.ACTIVE_THREAD.start()
                 self.LIGHT_ALARMS.append(alarm)
             elif task_type == Automation.LIGHT.TURN_OFF:
                 alarm = LightOnOffState(task_id, alarm_time, alarm_days)
-                alarm.ACTIVE_THREAD = create_thread(lambda: light_off_program(alarm, self.get_light_api_key(), light_group_id), Automation.TIME.TWENTY_SECONDS)
+                alarm.ACTIVE_THREAD = create_thread(lambda: light_off_program(alarm, self.get_light_api_key(), light_group_id), Automation.TIME.TEN_SECONDS)
                 alarm.ACTIVE_THREAD.start()
                 self.LIGHT_ALARMS.append(alarm)
 
