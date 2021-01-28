e0f0198ffa5098fb68b1d48fcf4ca74107e46d61
Jon Graf <jongraf1@gmail.com>
2021-01-28 11:42:08 -0600

    adding additonal logging as unable to determine what is going on with the light schedulers...

--- svc/utilities/api_utils.py
+++ svc/utilities/api_utils.py
@@ -23,12 +23,15 @@ def get_light_api_key(username, password):
 
 def set_light_groups(api_key, group_id, state, brightness=None):
     url = f'{Settings.get_instance().light_base_url}/{api_key}/groups/{group_id}/action'
+    logging.info(f'URL: {url}')
     request = {'on': state}
     if brightness is not None:
         request['on'] = True
         request['bri'] = brightness
     try:
-        requests.put(url, data=json.dumps(request))
+        logging.info(f'API Requested State: {state}')
+        response = requests.put(url, data=json.dumps(request))
+        logging.info(f'API Response Code: {response.status_code}  Content: {response.content}')
     except Exception:
         logging.error('Set Light Groups Put request threw an exception')
 
